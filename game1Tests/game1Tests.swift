6045edfc640d7bac58708b6be1217fd962ed9530
Tanay Mehta <tanaykmehta@gmail.com>
2025-03-17 15:28:38 +0530

    Initial Commit

---
+++ game1Tests/game1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  game1Tests.swift
+//  game1Tests
+//
+//  Created by Tanay Mehta on 17/03/25.
+//
+
+import Testing
+@testable import game1
+
+struct game1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
